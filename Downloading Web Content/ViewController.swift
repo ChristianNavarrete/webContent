30c1db6c2604e48b52947e72880792e804e558fc
HoodsDream <idinkyy@gmail.com>
2015-03-31 15:13:02 -0700

    hello

--- Downloading Web Content/ViewController.swift
+++ Downloading Web Content/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://www.stackoverflow.com")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {
+            
+            (data, response, error) in
+            
+                if error == nil {
+                    
+                    var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding)
+                    
+                    //makes sure that its executed on the same queue(same time basically)
+                    dispatch_async(dispatch_get_main_queue()) {
+                    
+                    self.webView.loadHTMLString(urlContent!, baseURL: nil)
+                        
+                    }
+                    
+                    println(urlContent)
+                }
+            
+            }
+            
+            task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
